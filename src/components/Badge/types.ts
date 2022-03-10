68bbc6a450bd2ac0f1cff12fe84fead02b0675a9
dmytrojfront <dmytro.j@lineir.me>
2022-03-10 16:25:44 +0200

    add badge

---
+++ src/components/Badge/types.ts
@@ -0,0 +1,7 @@
+export interface BadgeProps {
+  badgeType: "new" | "hot" | "light" | "dark" | "primary" | "warning" | string;
+  fontSize?: string;
+  fontWeight?: string;
+  isMenu?: boolean;
+  lineHeight?: string
+}
