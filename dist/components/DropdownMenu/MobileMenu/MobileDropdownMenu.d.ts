44d4f1c869a3dcf06a0c467be410b8da41b6cd63
Nathaniel <15807095+devNathaniel001@users.noreply.github.com>
2022-03-29 16:33:19 +0300

    fix icon

--- dist/components/DropdownMenu/MobileMenu/MobileDropdownMenu.d.ts
+++
@@ -1,4 +0,0 @@
-import { FC } from "react";
-import { MenuItemsProps } from "../../MenuItems/types";
-declare const MobileDropdownMenu: FC<MenuItemsProps>;
-export default MobileDropdownMenu;
--- src/components/Svg/index.tsx
+++ src/components/Svg/index.tsx
@@ -1,22 +1,21 @@
-export { default as AddIcon } from "./Icons/Add";
-export { default as ArrowBackIcon } from "./Icons/ArrowBack";
-export { default as ArrowDownIcon } from "./Icons/ArrowDown";
-export { default as ArrowDropDownIcon } from "./Icons/ArrowDropDown";
-export { default as ArrowDropUpIcon } from "./Icons/ArrowDropUp";
-export { default as ArrowForwardIcon } from "./Icons/ArrowForward";
+export {default as AddIcon} from "./Icons/Add";
+export {default as ArrowBackIcon} from "./Icons/ArrowBack";
+export {default as ArrowDownIcon} from "./Icons/ArrowDown";
+export {default as ArrowDropDownIcon} from "./Icons/ArrowDropDown";
+export {default as ArrowDropUpIcon} from "./Icons/ArrowDropUp";
+export {default as ArrowForwardIcon} from "./Icons/ArrowForward";
 export {default as ArrowUpIcon} from "./Icons/ArrowUp";
-export {default as ArrowUpForwardIcon} from "./Icons/ArrowUpForward";
 export {default as AutoRenewIcon} from "./Icons/AutoRenew";
-export { default as BinanceIcon } from "./Icons/Binance";
-export { default as BlockIcon } from "./Icons/Block";
-export { default as BunnyPlaceholderIcon } from "./Icons/BunnyPlaceholder";
-export { default as CardViewIcon } from "./Icons/CardView";
-export { default as CalculateIcon } from "./Icons/Calculate";
-export { default as CheckmarkIcon } from "./Icons/Checkmark";
-export { default as CheckmarkCircleIcon } from "./Icons/CheckmarkCircle";
-export { default as ChevronDownIcon } from "./Icons/ChevronDown";
-export { default as ChevronLeftIcon } from "./Icons/ChevronLeft";
-export { default as ChevronRightIcon } from "./Icons/ChevronRight";
+export {default as BinanceIcon} from "./Icons/Binance";
+export {default as BlockIcon} from "./Icons/Block";
+export {default as BunnyPlaceholderIcon} from "./Icons/BunnyPlaceholder";
+export {default as CardViewIcon} from "./Icons/CardView";
+export {default as CalculateIcon} from "./Icons/Calculate";
+export {default as CheckmarkIcon} from "./Icons/Checkmark";
+export {default as CheckmarkCircleIcon} from "./Icons/CheckmarkCircle";
+export {default as ChevronDownIcon} from "./Icons/ChevronDown";
+export {default as ChevronLeftIcon} from "./Icons/ChevronLeft";
+export {default as ChevronRightIcon} from "./Icons/ChevronRight";
 export { default as ChevronRightEndIcon } from "./Icons/ChevronRightEnd";
 export { default as ChevronUpIcon } from "./Icons/ChevronUp";
 export { default as CloseIcon } from "./Icons/Close";
@@ -29,7 +28,6 @@ export { default as InfoIcon } from "./Icons/Info";
 export { default as ListViewIcon } from "./Icons/ListView";
 export { default as TileViewIcon } from "./Icons/TileView";
 export { default as LogoIcon } from "./Icons/Logo";
-export { default as LogoWithTextIcon } from "./Icons/LogoWithText";
 export { default as LogoRoundIcon } from "./Icons/LogoRound";
 export { default as LoaderIcon } from "./Icons/Loader";
 export { default as MetamaskIcon } from "./Icons/Metamask";
@@ -38,16 +36,16 @@ export { default as PlusIcon } from "./Icons/Plus";
 export { default as NoProfileAvatarIcon } from "./Icons/NoProfileAvatar";
 export { default as OpenNewIcon } from "./Icons/OpenNew";
 export { default as PancakesIcon } from "./Icons/Pancakes";
-export { default as PancakeRoundIcon } from "./Icons/PancakeRound";
-export { default as PlayCircleOutlineIcon } from "./Icons/PlayCircleOutline";
-export { default as PrizeIcon } from "./Icons/Prize";
-export { default as RemoveIcon } from "./Icons/Remove";
-export { default as VerifiedIcon } from "./Icons/Verified";
-export { default as ProgressBunny } from "./Icons/ProgressBunny";
-export { default as WaitIcon } from "./Icons/Wait";
-export { default as Won } from "./Icons/Won";
-export { default as Ticket } from "./Icons/Ticket";
-export { default as TicketRound } from "./Icons/TicketRound";
+export {default as PancakeRoundIcon} from "./Icons/PancakeRound";
+export {default as PlayCircleOutlineIcon} from "./Icons/PlayCircleOutline";
+export {default as PrizeIcon} from "./Icons/Prize";
+export {default as RemoveIcon} from "./Icons/Remove";
+export {default as VerifiedIcon} from "./Icons/Verified";
+export {default as ProgressBunny} from "./Icons/ProgressBunny";
+export {default as WaitIcon} from "./Icons/Wait";
+export {default as Won} from "./Icons/Won";
+export {default as Ticket} from "./Icons/Ticket";
+export {default as TicketRound} from "./Icons/TicketRound";
 export {default as TimerIcon} from "./Icons/Timer";
 export {default as SearchIcon} from "./Icons/Search";
 export {default as SwapVertIcon} from "./Icons/SwapVert";
@@ -58,67 +56,40 @@ export {default as LinkIcon} from "./Icons/Link";
 export {default as TelegramIcon} from "./Icons/Telegram";
 export {default as TwitterIcon} from "./Icons/Twitter";
 export {default as MediumIcon} from "./Icons/Medium";
-export {default as RocketIcon} from "./Icons/Rocket";
-export {default as FlagIcon} from "./Icons/Flag";
-export {default as WalletIcon} from "./Icons/Wallet";
-export {default as WaitReloadIcon} from "./Icons/WaitReload";
-export {default as MouseIcon} from "./Icons/Mouse";
-export {default as RatingIcon} from "./Icons/Rating";
-export {default as AutoCompaundIcon} from "./Icons/AutoCompaund";
-export {default as FireIcon} from "./Icons/Fire";
-export {default as HourglassIcon} from "./Icons/Hourglass";
-export {default as GasIcon} from "./Icons/Gas";
-export {default as TradeIcon} from "./Icons/Trade";
-export {default as StarIcon} from "./Icons/Star";
-export {default as LevelIcon} from "./Icons/Level";
-export {default as RobiBoostIcon} from "./Icons/RobiBoost";
-export {default as MarketplaceIcon} from "./Icons/MarketPlace";
-export {default as FavoriteFullIcon} from "./Icons/FavoriteFull";
-export {default as FavoriteEmptyIcon} from "./Icons/FavoriteEmpty";
-export {default as ShareIcon} from "./Icons/Share";
-export {default as ViewIcon} from "./Icons/View";
-export {default as SettingsIcon} from "./Icons/Settings";
-export {default as MarketBagIcon} from "./Icons/MarketBag";
-export {default as IdoIcon} from "./Icons/Ido";
-export {default as SquidIcon} from "./Icons/Squid";
-export {default as SquidV2Icon} from "./Icons/SquidV2";
-export {default as PlayersIcon} from "./Icons/Players";
-export {default as SquidEnergyIcon} from "./Icons/SquidEnergy";
-export {default as VoteIcon} from "./Icons/Vote";
-export {default as EditIcon} from "./Icons/Edit";
-export {default as AddFilledIcon} from "./Icons/AddFilled";
-export {default as MoreHorizontal} from "./Icons/MoreHorizontal";
-
-export {default as ImgWarnIcon} from "./Icons/ImgWarn";
-
-export {default as WalletConnect} from "./Icons/WalletConnect";
-export {default as TrustWallet} from "./Icons/TrustWallet";
-export {default as MathWallet} from "./Icons/MathWallet";
-export {default as TokenPocket} from "./Icons/TokenPocket";
-export {default as BinanceChain} from "./Icons/BinanceChain";
-export {default as SafePal} from "./Icons/SafePal";
-export {default as Coin98} from "./Icons/Coin98";
-
-export {default as SwapIcon} from "./Icons/Swap";
-export {default as ExpertTradeIcon} from "./Icons/ExpertTrade";
-export {default as LiquidityIcon} from "./Icons/Liquidity";
-export {default as FarmsIcon} from "./Icons/Farms";
-export {default as LaunchIcon} from "./Icons/Launch";
-export {default as ReferralIcon} from "./Icons/Referral";
-export {default as IdoMenuIcon} from "./Icons/IdoMenu";
-export {default as LotteryIcon} from "./Icons/Lottery";
-export {default as CompetitionsIcon} from "./Icons/Competitions";
-export {default as NftLaunchIcon} from "./Icons/NftLaunch";
-export {default as NftEarnIcon} from "./Icons/NftEarn";
-export {default as MarketMenuIcon} from "./Icons/MarketMenu";
-export {default as GameFiIcon} from "./Icons/GameFi";
-export {default as ProductsIcon} from "./Icons/Products";
-export {default as ButtonMenuIcon} from "./Icons/ButtonMenu";
-export {default as ButtonMenuOpenedIcon} from "./Icons/ButtonMenuOpened";
-export {default as CerticAuditedIcon} from "./Icons/Certic";
-
-export {default as BscNewIcon} from './Icons/BscNew'
+export {default as RocketIcon} from "./Icons/Rocket"
+export {default as FlagIcon} from "./Icons/Flag"
+export {default as WalletIcon} from "./Icons/Wallet"
+export {default as WaitReloadIcon} from "./Icons/WaitReload"
+export {default as MouseIcon} from "./Icons/Mouse"
+export {default as RatingIcon} from "./Icons/Rating"
 export {default as CupIcon} from "./Icons/Cup"
+export {default as AutoCompaundIcon} from './Icons/AutoCompaund'
+export {default as FireIcon} from './Icons/Fire'
+export {default as HourglassIcon} from './Icons/Hourglass'
+export {default as GasIcon} from './Icons/Gas'
+export {default as TradeIcon} from './Icons/Trade'
+export {default as StarIcon} from './Icons/Star'
+export {default as LevelIcon} from './Icons/Level'
+export {default as RobiBoostIcon} from './Icons/RobiBoost'
+export {default as MarketplaceIcon} from './Icons/MarketPlace'
+export {default as FavoriteFullIcon} from './Icons/FavoriteFull'
+export {default as FavoriteEmptyIcon} from './Icons/FavoriteEmpty'
+export {default as ShareIcon} from './Icons/Share'
+export {default as ViewIcon} from './Icons/View'
+export {default as SettingsIcon} from './Icons/Settings'
+export {default as MarketBagIcon} from './Icons/MarketBag'
+export {default as IdoIcon} from './Icons/Ido'
+export {default as SquidIcon} from './Icons/Squid'
+export {default as SquidV2Icon} from './Icons/SquidV2'
+export {default as PlayersIcon} from './Icons/Players'
+export {default as SquidEnergyIcon} from './Icons/SquidEnergy'
+export {default as VoteIcon} from './Icons/Vote'
+export {default as EditIcon} from './Icons/Edit'
+export {default as AddFilledIcon} from './Icons/AddFilled'
+export {default as HolderAutoIcon} from './Icons/HolderAuto'
+export {default as BscNewIcon} from './Icons/BscNew'
+
+export {default as ImgWarnIcon} from './Icons/ImgWarn'
 
 //chain
 export {default as BSCIcon} from "./Icons/BSC";
