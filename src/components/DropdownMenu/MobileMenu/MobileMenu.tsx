5b7707dbab1e828165d49cce1a0853c6826ab184
dmytrojfront <dmytro.j@lineir.me>
2022-05-24 16:20:11 +0300

    mob menu fix

--- src/components/DropdownMenu/MobileMenu/MobileMenu.tsx
+++ src/components/DropdownMenu/MobileMenu/MobileMenu.tsx
@@ -19,8 +19,8 @@ const StyledMobileMenu = styled.div<{
   padding: 32px 24px 0 24px;
   background-color: ${({theme}) => theme.card.background};
   width: 100vw;
-  height: calc(100vh - 72px);
-  min-height: calc(100vh - 72px);
+  height: calc(100vh + 52px);
+  min-height: calc(100vh + 52px);
   overflow: auto;
   visibility: visible;
   opacity: 1;
