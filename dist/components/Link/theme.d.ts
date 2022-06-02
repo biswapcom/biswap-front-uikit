5c94afd6cb1fb9deb05da148ac577b3d32fcca21
Nathaniel <15807095+devNathaniel001@users.noreply.github.com>
2022-06-02 15:43:00 +0300

    fix link

---
+++ dist/components/Link/theme.d.ts
@@ -0,0 +1,43 @@
+export declare const scaleVariants: {
+    [x: string]: {
+        fontSize: string;
+    };
+};
+export declare const styleVariants: {
+    light: {
+        color: string;
+        svg: {
+            fill: string;
+        };
+        ":hover:not(:disabled)": {
+            color: string;
+            svg: {
+                fill: string;
+            };
+        };
+        ":active:not(:disabled)": {
+            color: string;
+            svg: {
+                fill: string;
+            };
+        };
+    };
+    dark: {
+        color: string;
+        svg: {
+            fill: string;
+        };
+        ":hover:not(:disabled)": {
+            color: string;
+            svg: {
+                fill: string;
+            };
+        };
+        ":active:not(:disabled)": {
+            color: string;
+            svg: {
+                fill: string;
+            };
+        };
+    };
+};
