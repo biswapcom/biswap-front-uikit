082e622cb01fa4dc2cc01841f7f9000e193f697f
Nathaniel <15807095+devNathaniel001@users.noreply.github.com>
2022-06-02 13:17:07 +0300

    resolves

---
+++ dist/components/DropdownMenu/components/index.d.ts
@@ -0,0 +1,4 @@
+import DropdownMenuItemContainer from "./DropdownMenuItemContainer";
+import InnerLinksBlock from "./InnerLinksBlock";
+import MenuItemContent from "./MenuItemContent";
+export { DropdownMenuItemContainer, InnerLinksBlock, MenuItemContent };
