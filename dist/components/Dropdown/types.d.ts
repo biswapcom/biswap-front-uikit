87ad24f04839e6d50233cfd5f2121f62e6038bce
Nathaniel <15807095+devNathaniel001@users.noreply.github.com>
2022-06-14 10:39:59 +0300

    resolves

---
+++ dist/components/Dropdown/types.d.ts
@@ -0,0 +1,33 @@
+import { SpaceProps } from "styled-system";
+export declare type Position = "top" | "bottom";
+export interface PositionProps {
+    position?: Position;
+}
+export declare type Variant = typeof variants[keyof typeof variants];
+export declare type Scale = typeof scales[keyof typeof scales];
+export interface DropdownProps extends PositionProps, SpaceProps {
+    maxWidth?: string;
+    minWidth?: string;
+    withIcon?: string;
+    variant?: Variant;
+    scale?: Scale;
+    disabled?: boolean;
+    options: Array<OptionProps>;
+    onChange?: (option: OptionProps) => void;
+}
+export interface OptionProps {
+    label: string;
+    icon?: {
+        name: string;
+        color: string;
+    };
+}
+export declare const scales: {
+    LG: string;
+    MD: string;
+    SM: string;
+};
+export declare const variants: {
+    DARK: string;
+    LIGHT: string;
+};
---
+++ dist/components/DropdownMenu/MobileMenu/ButtonBurger/Burger.styled.d.ts
@@ -0,0 +1,6 @@
+declare type StyledBurgerProps = {
+    open: boolean;
+    isLoaded: boolean;
+};
+export declare const StyledBurger: import("styled-components").StyledComponent<"button", import("styled-components").DefaultTheme, StyledBurgerProps, never>;
+export {};
