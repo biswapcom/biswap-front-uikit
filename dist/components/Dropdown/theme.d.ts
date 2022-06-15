0cabcd61a30af80dba30bc217fae6e12a9bf8beb
Nathaniel <15807095+devNathaniel001@users.noreply.github.com>
2022-06-15 11:41:48 +0300

    fix dropdown

---
+++ dist/components/Dropdown/theme.d.ts
@@ -0,0 +1,62 @@
+export declare const scaleVariantsTop: {
+    [x: string]: {
+        height: string;
+        padding: string;
+        fontSize: string;
+        borderRadius: string;
+        svg: {
+            width: string;
+        };
+    };
+};
+export declare const scaleVariantsContent: {
+    [x: string]: {
+        borderRadius: string;
+    };
+};
+export declare const scaleVariantItem: {
+    [x: string]: {
+        height: string;
+        fontSize: string;
+        padding: string;
+        svg: {
+            width: string;
+            marginRight: string;
+        };
+    };
+};
+export declare const styleVariantsTop: {
+    [x: string]: {
+        borderColor: string;
+        color: string;
+        ".arrow": {
+            fill: string;
+        };
+        ":hover": {
+            color: string;
+            borderColor: string;
+        };
+        "&.open": {
+            color: string;
+            borderColor: string;
+            ":hover": {
+                color: string;
+                borderColor: string;
+            };
+            ".arrow": {
+                fill: string;
+            };
+        };
+        "&.disabled": {
+            color: string;
+            borderColor: string;
+            ":hover": {
+                color: string;
+                borderColor: string;
+            };
+            ".arrow": {
+                fill: string;
+            };
+        };
+    };
+};
---
+++ dist/components/Radio/Radio.d.ts
@@ -0,0 +1,4 @@
+import { FC } from "react";
+import { RadioProps } from "./types";
+declare const Radio: FC<RadioProps>;
+export default Radio;
