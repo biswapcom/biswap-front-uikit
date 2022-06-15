eceda3120013e108e678a497adb7f2ae3e6f90ee
andkfront <andriy.k@lineir.me>
2022-06-15 09:52:30 +0300

    tab menu fix

--- src/components/TabMenu/TabBarItem.tsx
+++ src/components/TabMenu/TabBarItem.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect, useRef } from "react";
+import React, { FC, useEffect } from "react";
 import styled from "styled-components";
 import { PolymorphicComponent } from "../../util/polymorphic";
 import { BaseButtonProps } from "../Button/types";
