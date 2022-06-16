27b08729d1cc062b3fb2565ff76a979b51be1bd8
Nathaniel <15807095+devNathaniel001@users.noreply.github.com>
2022-06-16 16:14:26 +0300

    fix dropdown

--- src/components/Dropdown/types.ts
+++ src/components/Dropdown/types.ts
@@ -22,6 +22,7 @@ export interface DropdownProps extends PositionProps, SpaceProps {
 
 export interface OptionProps {
   label: string;
+  value?: string;
   icon?: {
     name: string;
     color?: string;
