b86f4f95cbc84cd284dcc0f581257a86d9f3ff5e
dmytrojfront <dmytro.j@lineir.me>
2022-06-24 16:46:37 +0300

    fix popup title color

--- src/components/Heading/Heading.tsx
+++ src/components/Heading/Heading.tsx
@@ -22,7 +22,6 @@ const style = {
 };
 
 const Heading = styled(Text).attrs({ bold: true })<HeadingProps>`
-  color: ${({theme}) => theme.colors.backgroundDark};
   font-size: ${({ scale }) => style[scale || scales.MD].fontSize};
   font-weight: 600;
   line-height: 1.1;
