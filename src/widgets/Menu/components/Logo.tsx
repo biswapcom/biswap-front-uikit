319e935c2d3ca61a81b89816dfc071145f869f6e
front_O <oleg.k@lineir.me>
2022-07-20 16:40:01 +0300

    try fix useHistory

--- src/widgets/Menu/components/Logo.tsx
+++ src/widgets/Menu/components/Logo.tsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { useHistory } from "react-router-dom";
+// import { useHistory } from "react-router-dom";
 import styled, { css, DefaultTheme, keyframes } from "styled-components";
 
 // components
@@ -58,7 +58,7 @@ const StyledInnerButton = styled(Button)`
 
 const Logo: React.FC<Props> = ({ href }) => {
   const { linkComponent } = useContext(MenuContext);
-  const { push } = useHistory();
+  // const { push } = useHistory();
 
   const isAbsoluteUrl = href.startsWith("http");
   const innerLogo = (
@@ -81,7 +81,9 @@ const Logo: React.FC<Props> = ({ href }) => {
       ) : (
         <StyledInnerButton
           variant="light"
-          onClick={() => push(href)}
+          as={linkComponent}
+          href={href}
+          // onClick={() => push(href)}
           aria-label="Biswap home page"
         >
           {innerLogo}
