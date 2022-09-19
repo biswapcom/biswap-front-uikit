6363249ce442d5ca5c11a143e144f1d99a7fe03a
Nathaniel <15807095+devNathaniel001@users.noreply.github.com>
2022-09-19 12:47:51 +0300

    skeleton

---
+++ dist/components/TabMenu/types.d.ts
@@ -0,0 +1,32 @@
+import { SpaceProps } from "styled-system";
+import { Dispatch, ElementType, SetStateAction, ReactNode } from "react";
+import { PolymorphicComponentProps } from "../../util/polymorphic";
+export interface TabBarProps extends SpaceProps {
+    variant?: typeof tabVariants.DARK | typeof tabVariants.LIGHT;
+    activeIndex?: number;
+    scale?: typeof tabsScales[keyof typeof tabsScales];
+    disabled?: boolean;
+    fullWidth?: boolean;
+    menuTitles?: Array<string>;
+    menuIcons?: Array<string>;
+    customClass?: string;
+    isLight?: boolean;
+}
+export interface TabBarItemProps extends TabBarProps {
+    isActive?: boolean;
+    setWidth?: Dispatch<SetStateAction<any>>;
+    itemIndex?: number;
+    children?: ReactNode;
+    onAction?: (index: number) => void;
+    blockOffset?: number;
+}
+export declare type TabProps<P extends ElementType = "button"> = PolymorphicComponentProps<P, TabBarItemProps>;
+export declare const tabsScales: {
+    readonly LG: "lg";
+    readonly MD: "md";
+    readonly SM: "sm";
+};
+export declare const tabVariants: {
+    readonly DARK: "dark";
+    readonly LIGHT: "light";
+};
