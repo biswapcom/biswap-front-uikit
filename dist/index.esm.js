aede6172a07ab166a953f09207984e2250a563f9
artemchfrontbs <artem.ch@lineir.me>
2022-10-17 18:14:50 +0300

    fix footer padding

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -8124,7 +8124,7 @@ var Audit = function () { return (React.createElement(AuditedWrap, null,
             } })))); };
 var templateObject_1$n, templateObject_2$f;
 
-var Wrapper$2 = styled.footer(templateObject_1$m || (templateObject_1$m = __makeTemplateObject(["\n  color: #fff;\n  background: ", ";\n  padding: 32px 16px;\n  transition: padding-left 0.2s;\n  z-index: 10;\n"], ["\n  color: #fff;\n  background: ", ";\n  padding: 32px 16px;\n  transition: padding-left 0.2s;\n  z-index: 10;\n"])), function (_a) {
+var Wrapper$2 = styled.footer(templateObject_1$m || (templateObject_1$m = __makeTemplateObject(["\n  color: #fff;\n  background: ", ";\n  padding: 56px 16px 32px;\n  transition: padding-left 0.2s;\n  z-index: 10;\n"], ["\n  color: #fff;\n  background: ", ";\n  padding: 56px 16px 32px;\n  transition: padding-left 0.2s;\n  z-index: 10;\n"])), function (_a) {
     var theme = _a.theme;
     return theme.colors.dark700;
 });
