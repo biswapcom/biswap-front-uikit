1054a48c29936caf6538daf9c485a9d2f66f22d9
vladmbs <vlad.m@lineir.me>
2022-10-20 10:05:05 +0300

    add line height for text

--- dist/components/Typography/BodyText.d.ts
+++ dist/components/Typography/BodyText.d.ts
@@ -1,5 +1,3 @@
 import { BodyTextProps } from "./typesBodyText";
-declare const BodyText: import("styled-components").StyledComponent<"div", import("styled-components").DefaultTheme, import("../Text").TextProps & {
-    bold: boolean;
-} & BodyTextProps, "bold">;
+declare const BodyText: import("styled-components").StyledComponent<"div", import("styled-components").DefaultTheme, import("../Text").TextProps & BodyTextProps, never>;
 export default BodyText;
