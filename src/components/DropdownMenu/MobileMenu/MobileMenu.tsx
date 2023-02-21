a21a9c2821e29ad203631a503d7add000cb1e3c7
artemchfrontbs <artem.ch@lineir.me>
2023-02-21 14:59:18 +0200

    fix social links in menu

--- src/components/DropdownMenu/MobileMenu/MobileMenu.tsx
+++ src/components/DropdownMenu/MobileMenu/MobileMenu.tsx
@@ -20,8 +20,6 @@ const MobileCommunityWrapper = styled.div`
   display: flex;
   flex-direction: column;
   margin-top: 32px;
-  //padding: 0 54px;
-  //background: green;
 `;
 
 const StyledMobileMenu = styled.div<{
