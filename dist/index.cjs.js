59f05581c3d874da91304357fbc9ddc22a631a6f
front_O <oleg.k@lineir.me>
2023-08-08 16:41:22 +0300

    fix icon

--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -1448,17 +1448,17 @@ var Icon$v = function (props) {
 
 var Icon$u = function (props) {
     return (React__default["default"].createElement(Svg, __assign({ viewBox: "0 0 24 24", fill: "currentColor" }, props),
-        React__default["default"].createElement("g", { clipPath: "url(#clip0_11573_34525)" },
-            React__default["default"].createElement("path", { opacity: "0.48", d: "M24 11.7293C24 5.25146 18.6274 0 12 0C5.37264 0 0 5.25146 0 11.7293C0 11.8199 0.00125523 11.9099 0.0032636 12C0.00125523 12.0901 0 12.1801 0 12.2707C0 18.7485 5.37264 24 12 24C18.6274 24 24 18.7485 24 12.2707C24 12.1801 23.9987 12.0901 23.9967 12C23.9987 11.9099 24 11.8199 24 11.7293Z" }),
-            React__default["default"].createElement("path", { d: "M6.34644 12.1668L11.9984 9.65584V3.00012L6.34644 12.1668Z" }),
-            React__default["default"].createElement("path", { opacity: "0.56", d: "M12 3V9.65572L17.6523 12.1667L12 3Z" }),
-            React__default["default"].createElement("path", { opacity: "0.56", d: "M11.9983 9.65601V15.4326L17.6506 12.167L11.9983 9.65601Z" }),
-            React__default["default"].createElement("path", { d: "M6.34644 13.2141L11.9984 20.9994V16.478L6.34644 13.2141Z" }),
-            React__default["default"].createElement("path", { opacity: "0.56", d: "M12 16.478V20.9994L17.6557 13.2141L12 16.478Z" }),
-            React__default["default"].createElement("path", { d: "M6.34644 12.167L11.9984 15.4326V9.65601L6.34644 12.167Z" })),
+        React__default["default"].createElement("g", { "clip-path": "url(#clip0_1_4372)" },
+            React__default["default"].createElement("path", { d: "M24 11.7294C24 5.25152 18.6274 6.10352e-05 12 6.10352e-05C5.37264 6.10352e-05 0 5.25152 0 11.7294C0 11.8199 0.00125523 11.91 0.0032636 12.0001C0.00125523 12.0901 0 12.1802 0 12.2707C0 18.7486 5.37264 24.0001 12 24.0001C18.6274 24.0001 24 18.7486 24 12.2707C24 12.1802 23.9987 12.0901 23.9967 12.0001C23.9987 11.91 24 11.8199 24 11.7294Z", fill: "white" }),
+            React__default["default"].createElement("path", { d: "M7.18616 12.1421L11.9985 10.0041V4.3371L7.18616 12.1421Z", fill: "#1263F1" }),
+            React__default["default"].createElement("path", { d: "M11.9984 4.33698V10.004L16.811 12.1419L11.9984 4.33698Z", fill: "#1263F1", "fill-opacity": "0.48" }),
+            React__default["default"].createElement("path", { d: "M11.9984 10.0042V14.9226L16.811 12.1422L11.9984 10.0042Z", fill: "#1263F1", "fill-opacity": "0.32" }),
+            React__default["default"].createElement("path", { d: "M7.18616 13.0338L11.9985 19.6625V15.8128L7.18616 13.0338Z", fill: "#1263F1" }),
+            React__default["default"].createElement("path", { d: "M11.9984 15.8128V19.6625L16.8139 13.0338L11.9984 15.8128Z", fill: "#1263F1", "fill-opacity": "0.48" }),
+            React__default["default"].createElement("path", { d: "M7.18616 12.1422L11.9985 14.9226V10.0042L7.18616 12.1422Z", fill: "#1263F1", "fill-opacity": "0.82" })),
         React__default["default"].createElement("defs", null,
-            React__default["default"].createElement("clipPath", { id: "clip0_11573_34525" },
-                React__default["default"].createElement("rect", { width: "100%", height: "100%" })))));
+            React__default["default"].createElement("clipPath", { id: "clip0_1_4372" },
+                React__default["default"].createElement("rect", { width: "24", height: "24", fill: "white" })))));
 };
 
 var Icon$t = function (props) {
--- dist/index.esm.js
+++ dist/index.esm.js
@@ -1432,17 +1432,17 @@ var Icon$v = function (props) {
 
 var Icon$u = function (props) {
     return (React.createElement(Svg, __assign({ viewBox: "0 0 24 24", fill: "currentColor" }, props),
-        React.createElement("g", { clipPath: "url(#clip0_11573_34525)" },
-            React.createElement("path", { opacity: "0.48", d: "M24 11.7293C24 5.25146 18.6274 0 12 0C5.37264 0 0 5.25146 0 11.7293C0 11.8199 0.00125523 11.9099 0.0032636 12C0.00125523 12.0901 0 12.1801 0 12.2707C0 18.7485 5.37264 24 12 24C18.6274 24 24 18.7485 24 12.2707C24 12.1801 23.9987 12.0901 23.9967 12C23.9987 11.9099 24 11.8199 24 11.7293Z" }),
-            React.createElement("path", { d: "M6.34644 12.1668L11.9984 9.65584V3.00012L6.34644 12.1668Z" }),
-            React.createElement("path", { opacity: "0.56", d: "M12 3V9.65572L17.6523 12.1667L12 3Z" }),
-            React.createElement("path", { opacity: "0.56", d: "M11.9983 9.65601V15.4326L17.6506 12.167L11.9983 9.65601Z" }),
-            React.createElement("path", { d: "M6.34644 13.2141L11.9984 20.9994V16.478L6.34644 13.2141Z" }),
-            React.createElement("path", { opacity: "0.56", d: "M12 16.478V20.9994L17.6557 13.2141L12 16.478Z" }),
-            React.createElement("path", { d: "M6.34644 12.167L11.9984 15.4326V9.65601L6.34644 12.167Z" })),
+        React.createElement("g", { "clip-path": "url(#clip0_1_4372)" },
+            React.createElement("path", { d: "M24 11.7294C24 5.25152 18.6274 6.10352e-05 12 6.10352e-05C5.37264 6.10352e-05 0 5.25152 0 11.7294C0 11.8199 0.00125523 11.91 0.0032636 12.0001C0.00125523 12.0901 0 12.1802 0 12.2707C0 18.7486 5.37264 24.0001 12 24.0001C18.6274 24.0001 24 18.7486 24 12.2707C24 12.1802 23.9987 12.0901 23.9967 12.0001C23.9987 11.91 24 11.8199 24 11.7294Z", fill: "white" }),
+            React.createElement("path", { d: "M7.18616 12.1421L11.9985 10.0041V4.3371L7.18616 12.1421Z", fill: "#1263F1" }),
+            React.createElement("path", { d: "M11.9984 4.33698V10.004L16.811 12.1419L11.9984 4.33698Z", fill: "#1263F1", "fill-opacity": "0.48" }),
+            React.createElement("path", { d: "M11.9984 10.0042V14.9226L16.811 12.1422L11.9984 10.0042Z", fill: "#1263F1", "fill-opacity": "0.32" }),
+            React.createElement("path", { d: "M7.18616 13.0338L11.9985 19.6625V15.8128L7.18616 13.0338Z", fill: "#1263F1" }),
+            React.createElement("path", { d: "M11.9984 15.8128V19.6625L16.8139 13.0338L11.9984 15.8128Z", fill: "#1263F1", "fill-opacity": "0.48" }),
+            React.createElement("path", { d: "M7.18616 12.1422L11.9985 14.9226V10.0042L7.18616 12.1422Z", fill: "#1263F1", "fill-opacity": "0.82" })),
         React.createElement("defs", null,
-            React.createElement("clipPath", { id: "clip0_11573_34525" },
-                React.createElement("rect", { width: "100%", height: "100%" })))));
+            React.createElement("clipPath", { id: "clip0_1_4372" },
+                React.createElement("rect", { width: "24", height: "24", fill: "white" })))));
 };
 
 var Icon$t = function (props) {
