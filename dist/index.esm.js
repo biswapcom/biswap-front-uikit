ef5a52ce14c77dc8d75d2d31c08ae44497c5749d
front_O <oleg.k@lineir.me>
2023-08-08 16:55:47 +0300

    fix icon2

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -1432,14 +1432,14 @@ var Icon$v = function (props) {
 
 var Icon$u = function (props) {
     return (React.createElement(Svg, __assign({ viewBox: "0 0 24 24", fill: "currentColor" }, props),
-        React.createElement("g", { "clip-path": "url(#clip0_1_4372)" },
+        React.createElement("g", { clipPath: "url(#clip0_1_4372)" },
             React.createElement("path", { d: "M24 11.7294C24 5.25152 18.6274 6.10352e-05 12 6.10352e-05C5.37264 6.10352e-05 0 5.25152 0 11.7294C0 11.8199 0.00125523 11.91 0.0032636 12.0001C0.00125523 12.0901 0 12.1802 0 12.2707C0 18.7486 5.37264 24.0001 12 24.0001C18.6274 24.0001 24 18.7486 24 12.2707C24 12.1802 23.9987 12.0901 23.9967 12.0001C23.9987 11.91 24 11.8199 24 11.7294Z", fill: "white" }),
             React.createElement("path", { d: "M7.18616 12.1421L11.9985 10.0041V4.3371L7.18616 12.1421Z", fill: "#1263F1" }),
-            React.createElement("path", { d: "M11.9984 4.33698V10.004L16.811 12.1419L11.9984 4.33698Z", fill: "#1263F1", "fill-opacity": "0.48" }),
-            React.createElement("path", { d: "M11.9984 10.0042V14.9226L16.811 12.1422L11.9984 10.0042Z", fill: "#1263F1", "fill-opacity": "0.32" }),
+            React.createElement("path", { d: "M11.9984 4.33698V10.004L16.811 12.1419L11.9984 4.33698Z", fill: "#1263F1", fillOpacity: "0.48" }),
+            React.createElement("path", { d: "M11.9984 10.0042V14.9226L16.811 12.1422L11.9984 10.0042Z", fill: "#1263F1", fillOpacity: "0.32" }),
             React.createElement("path", { d: "M7.18616 13.0338L11.9985 19.6625V15.8128L7.18616 13.0338Z", fill: "#1263F1" }),
-            React.createElement("path", { d: "M11.9984 15.8128V19.6625L16.8139 13.0338L11.9984 15.8128Z", fill: "#1263F1", "fill-opacity": "0.48" }),
-            React.createElement("path", { d: "M7.18616 12.1422L11.9985 14.9226V10.0042L7.18616 12.1422Z", fill: "#1263F1", "fill-opacity": "0.82" })),
+            React.createElement("path", { d: "M11.9984 15.8128V19.6625L16.8139 13.0338L11.9984 15.8128Z", fill: "#1263F1", fillOpacity: "0.48" }),
+            React.createElement("path", { d: "M7.18616 12.1422L11.9985 14.9226V10.0042L7.18616 12.1422Z", fill: "#1263F1", fillOpacity: "0.82" })),
         React.createElement("defs", null,
             React.createElement("clipPath", { id: "clip0_1_4372" },
                 React.createElement("rect", { width: "24", height: "24", fill: "white" })))));
