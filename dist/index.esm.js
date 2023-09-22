5218e0424b60d2fb3c5185f74eb972ae1cad34ae
front_O <oleg.k@lineir.me>
2023-09-22 12:01:47 +0300

    change Twitter icon

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -1492,7 +1492,7 @@ var Icon$p = function (props) {
 
 var Icon$o = function (props) {
     return (React.createElement(Svg, __assign({ viewBox: "0 0 24 24", fill: "currentColor" }, props),
-        React.createElement("path", { d: "M18.244 2.25H21.552L14.325 10.51L22.827 21.75H16.17L10.956 14.933L4.99 21.75H1.68L9.41 12.915L1.254 2.25H8.08L12.793 8.481L18.244 2.25ZM17.083 19.77H18.916L7.084 4.126H5.117L17.083 19.77Z", fill: "#556E9A" })));
+        React.createElement("path", { d: "M18.244 2.25H21.552L14.325 10.51L22.827 21.75H16.17L10.956 14.933L4.99 21.75H1.68L9.41 12.915L1.254 2.25H8.08L12.793 8.481L18.244 2.25ZM17.083 19.77H18.916L7.084 4.126H5.117L17.083 19.77Z" })));
 };
 
 var Icon$n = function (props) {
