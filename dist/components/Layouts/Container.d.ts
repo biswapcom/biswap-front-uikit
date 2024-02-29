525042f3e2fe66eedd91c4620a2df26f5fb3f82b
andkfront <andriy.k@lineir.me>
2024-02-29 10:27:50 +0200

    revert changes

--- dist/components/Layouts/Container.d.ts
+++ dist/components/Layouts/Container.d.ts
@@ -1,8 +1,4 @@
+import React from "react";
 import { BoxProps } from "../Box";
-declare const Container: import("styled-components").StyledComponent<"div", import("styled-components").DefaultTheme, BoxProps & {
-    width: string;
-    px: any;
-    mx: string;
-    maxWidth: string;
-}, "width" | "maxWidth" | "mx" | "px">;
+declare const Container: React.FC<BoxProps>;
 export default Container;
