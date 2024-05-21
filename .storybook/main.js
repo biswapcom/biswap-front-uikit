9160a422245d3d6e3834e5e7e687207f0f3338f1
devStieg <oleg.k@biswap.dev>
2024-05-21 10:08:57 +0300

    final save

--- .storybook/main.js
+++ .storybook/main.js
@@ -21,49 +21,6 @@ module.exports = {
     ],
     typescript: {
         reactDocgen: 'react-docgen-typescript-plugin',
-        reactDocgenTypescriptOptions: {
-            propFilter: (prop) => {
-                if (prop.name === 'children') {
-                    return true;
-                }
-
-                if (prop.parent) {
-                    return (
-                        !/@types\/react/.test(prop.parent.fileName) &&
-                        !/@emotion/.test(prop.parent.fileName)
-                    );
-                }
-                return true;
-            },
-        },
+        reactDocgenTypescriptOptions: {},
     },
-    webpackFinal: async config => {
-        config.module.rules.push({
-            test: /\.(ts|tsx)$/,
-            exclude: /node_modules/,
-            use: [
-                {
-                    loader: require.resolve('ts-loader'),
-                    options: {
-                        configFile: '../tsconfig.json',
-                        transpileOnly: true
-                    }
-                },
-                {
-                    loader: require.resolve('babel-loader'),
-                    options: {
-                        presets: [
-                            "@babel/preset-env",
-                            "@babel/preset-react",
-                            "@babel/preset-typescript"
-                        ]
-                    }
-                }
-            ],
-        });
-
-        config.resolve.extensions.push('.ts', '.tsx');
-
-        return config;
-    }
 };
--- src/components/Layouts/ContainerCarousel.tsx
+++ src/components/Layouts/ContainerCarousel.tsx
@@ -1,4 +1,4 @@
-import React, {FC, ReactElement} from "react";
+import React, { FC, ReactElement } from "react";
 import { Box, BoxProps } from "../Box";
 import { getResponsiveAttrs } from "../../util";
 
