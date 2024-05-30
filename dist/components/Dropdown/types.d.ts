8e5c4f8a716e159ba68dbc20d3d5e156f6cc3092
Nathaniel <yaroslav.v@biswap.dev>
2024-05-30 12:54:07 +0300

    update dropdown

--- dist/components/Dropdown/types.d.ts
+++ dist/components/Dropdown/types.d.ts
@@ -16,7 +16,6 @@ export interface DropdownProps extends PositionProps, SpaceProps {
     options: Array<OptionProps>;
     onChange?: (option: OptionProps) => void;
     children?: ReactNode;
-    hideLabel?: boolean;
 }
 export interface OptionProps {
     label: string;
--- dist/index.esm.js
+++ dist/index.esm.js
@@ -4043,7 +4043,7 @@ var StyledNextImg = styled(Image$1)(templateObject_9$4 || (templateObject_9$4 = 
     return theme.mediaQueries.lg;
 });
 var Dropdown = function (_a) {
-    var _b = _a.position, position = _b === void 0 ? "bottom" : _b; _a.children; var maxWidth = _a.maxWidth, minWidth = _a.minWidth, scale = _a.scale, variant = _a.variant, disabled = _a.disabled, options = _a.options, onChange = _a.onChange, hideLabel = _a.hideLabel, props = __rest(_a, ["position", "children", "maxWidth", "minWidth", "scale", "variant", "disabled", "options", "onChange", "hideLabel"]);
+    var _b = _a.position, position = _b === void 0 ? "bottom" : _b; _a.children; var maxWidth = _a.maxWidth, minWidth = _a.minWidth, scale = _a.scale, variant = _a.variant, disabled = _a.disabled, options = _a.options, onChange = _a.onChange, props = __rest(_a, ["position", "children", "maxWidth", "minWidth", "scale", "variant", "disabled", "options", "onChange"]);
     var _c = useState(false), isOpen = _c[0], setIsOpen = _c[1];
     var wrapperRef = useRef(null);
     var _d = useState(options[0]), selectedOption = _d[0], setSelectedOption = _d[1];
@@ -4089,7 +4089,7 @@ var Dropdown = function (_a) {
                 React.createElement(StyledNextImg, { src: selectedOption.icon.name, width: scaleVariantsImage(scale), height: scaleVariantsImage(scale), quality: 90, alt: "icon" })
                 :
                     React.createElement(IconComponent$1, { iconName: selectedOption.icon.name, color: selectedOption.icon.color, mr: scale === "lg" ? "12px" : "8px" })),
-            !hideLabel && React.createElement(Label$1, null, selectedOption.label),
+            React.createElement(Label$1, null, selectedOption.label),
             React.createElement(StyledArrow$1, { className: "arrow", isOpen: isOpen })),
         isOpen && (React.createElement(DropdownContent$1, { position: position, scale: scale }, options.map(function (option) { return (React.createElement(DropdownItem$1, { scale: scale, selected: option.label === selectedOption.label, onClick: onOptionClicked(option), key: option.label },
             option.icon && (option.icon.isAws ?
